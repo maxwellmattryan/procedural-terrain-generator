5a01a46e3d9932507b25ae69c58541d1f806e1eb
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-04-13 15:11:40 -0500

    Implement noise map generator displaying basic perlin noise

---
+++ ProceduralTerrainGenerator/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        var display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+    public void CheckParameters()
+    {
+        if (mapWidth <= 0)
+            mapWidth = 1;
+        if (mapHeight <= 0)
+            mapHeight = 1;
+    }
+}
---
+++ ProceduralTerrainGenerator/Assets/Scripts/Noise.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    // TODO: write description comments somewhere (i.e. scale 
+    public static float[,] GenerateNoiseMap(int width, int height, float scale)
+    {
+        if (scale <= 0f)
+            scale = 0.001f;
+
+        float[,] noiseMap = new float[width, height];
+
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            { 
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float noiseValue = Mathf.PerlinNoise(sampleX, sampleY);
+
+                noiseMap[x, y] = noiseValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
